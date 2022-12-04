f34a86731f6d4f67d511aa6fa8bb51af94d162cc
linjunliang <236841563@qq.com>
2022-12-04 18:25:07 +0800

    init: 初始化

---
+++ lifelong-learning-consumer/src/main/java/com/lin/lifelong/learning/consumer/LifelongLearningConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lin.lifelong.learning.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelongLearningConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifelongLearningConsumerApplication.class, args);
+    }
+
+}
---
+++ lifelong-learning-consumer/src/test/java/com/lin/lifelong/learning/consumer/LifelongLearningConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.lifelong.learning.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelongLearningConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
